960e522f0ea162ec5f330ed175962910a535bfb7
kovalov-helfie <Serhii.Kovalov@helfie.ai>
2025-02-04 10:41:50 +0200

    feat(vault): addr

--- packages/nextjs/generated/deployedContracts.ts
+++ packages/nextjs/generated/deployedContracts.ts
@@ -15,6 +15,8 @@ import { TIR_ABI } from "./abis/tir.abi";
 import { TOKEN_FACTORY_ABI } from "./abis/token-factory.abi";
 import { TOKEN_ABI } from "./abis/token.abi";
 import { USDT_ABI } from "./abis/usdt.abi";
+import { VAULT_FACTORY_ABI } from "./abis/vault-factory.abi";
+import { VAULT_ABI } from "./abis/vault.abi";
 
 const contracts = {
   11155111: [
@@ -23,67 +25,75 @@ const contracts = {
       name: "sepolia",
       contracts: {
         Admin: {
-          address: "0xdc8d310adB5fC4ceF45ec4Fa9bdB7d9E3CEA6CEe",
+          address: "0x3aBcC74126978188353e740006E0CF788205594B",
           abi: ADMIN_ABI,
         },
         TrustedIssuersRegistry: {
-          address: "0xdc8d310adB5fC4ceF45ec4Fa9bdB7d9E3CEA6CEe",
+          address: "0x3aBcC74126978188353e740006E0CF788205594B",
           abi: TIR_ABI,
         },
         ClaimTopicsRegistry: {
-          address: "0xdc8d310adB5fC4ceF45ec4Fa9bdB7d9E3CEA6CEe",
+          address: "0x3aBcC74126978188353e740006E0CF788205594B",
           abi: CTR_ABI,
         },
         IdentityRegistryStorage: {
-          address: "0xdc8d310adB5fC4ceF45ec4Fa9bdB7d9E3CEA6CEe",
+          address: "0x3aBcC74126978188353e740006E0CF788205594B",
           abi: IRS_ABI,
         },
         IdentityRegistry: {
-          address: "0xdc8d310adB5fC4ceF45ec4Fa9bdB7d9E3CEA6CEe",
+          address: "0x3aBcC74126978188353e740006E0CF788205594B",
           abi: IR_ABI,
         },
         OperatorManagement: {
-          address: "0xdc8d310adB5fC4ceF45ec4Fa9bdB7d9E3CEA6CEe",
+          address: "0x3aBcC74126978188353e740006E0CF788205594B",
           abi: OM_ABI,
         },
         BalanceManagement: {
-          address: "0xdc8d310adB5fC4ceF45ec4Fa9bdB7d9E3CEA6CEe",
+          address: "0x3aBcC74126978188353e740006E0CF788205594B",
           abi: BM_ABI,
         },
         IdFactory: {
-          address: "0xdc8d310adB5fC4ceF45ec4Fa9bdB7d9E3CEA6CEe",
+          address: "0x3aBcC74126978188353e740006E0CF788205594B",
           abi: ID_FACTORY_ABI,
         },
         ModularCompliance: {
-          address: "0x390AB078171c81878e926D3b06C27E4878e969EB",
+          address: "0x4e203DDe594b9Ba94fa001E6dD3AfeA47D623094",
           abi: MODULAR_COMPLIANCE_ABI,
         },
         Token: {
-          address: "0x050E3D0e794c49F030B2894aeBDba6fB894C0262",
+          address: "0x108e9fFacca75fb7ec56D5fCC38c5e46883faCa2",
           abi: TOKEN_ABI,
         },
         Identity: {
-          address: "0xA3B221B86b66E473b5d9c92062e44863EaD61671",
+          address: "0xB3441E4b362F2578870163e9C9b0c58Af9bd1AE7",
           abi: IDENTITY_ABI,
         },
         CountryAllowModule: {
-          address: "0x411178be519cd438Fad80AF22f515aA718b22bBA",
+          address: "0x5ebc85bbb279dF5d40E2f1563A0621A472a8EBC5",
           abi: COUNTRY_ALLOW_ABI,
         },
         CountryRestrictModule: {
-          address: "0xB922c47Ed24CB62e8578683589919336ECC990d9",
+          address: "0xF856116c787E760606B6ed8285aE4c914E415B62",
           abi: COUNTRY_RESTRICT_ABI,
         },
         TimeTransferModule: {
-          address: "0x657bB38560c482BF8F4e9fAACd59905EE678e48f",
+          address: "0xA7b61FAE94a0935BD52A725D7c74CbeFeC4c0978",
           abi: TIME_TRANSFER_LIMIT_ABI,
         },
         TokenFactory: {
-          address: "0xdc8d310adB5fC4ceF45ec4Fa9bdB7d9E3CEA6CEe",
+          address: "0x3aBcC74126978188353e740006E0CF788205594B",
           abi: TOKEN_FACTORY_ABI,
         },
+        Vault: {
+          address: "0x3CcEc1ebC2f654392aC709383Ab18a3129Ce148F",
+          abi: VAULT_ABI,
+        },
+        VaultFactory: {
+          address: "0x3aBcC74126978188353e740006E0CF788205594B",
+          abi: VAULT_FACTORY_ABI,
+        },
         DVDTransferManager: {
-          address: "0xdc8d310adB5fC4ceF45ec4Fa9bdB7d9E3CEA6CEe",
+          address: "0x3aBcC74126978188353e740006E0CF788205594B",
           abi: DVD_ABI,
         },
         USDT: {
